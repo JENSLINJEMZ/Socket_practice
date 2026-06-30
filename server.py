a872d44e49e8269aaf1b350d265ae65da86d41e9
JENSLINJEMZ <jenslinjemz123@gmail.com>
2026-06-30 21:04:39 +0530

    add threadings to the server

--- server.py
+++ server.py
@@ -1,26 +1,47 @@
 import socket
-
+import threading
 server_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = "0.0.0.0"
 port = 4444
 
 server_soc.bind((host,port))
-
-server_soc.listen(1)
+server_soc.listen(5)
 print(f"[*] Server listening on {host}:{port}")
 print(f"[*] Waiting for connections...")
+def server():
+    try:
+        while True:
+            client_soc , address = server_soc.accept()
+            print(f"Connected from {address[0]}:{address[1]}")
+            client_soc.send('Hello Client'.encode())
+            while True:
+                data = client_soc.recv(1024).decode()
+                print("Client: ",data)
+                if not data:
+                    print("client not responding...")
+                    client_soc.close()
+                    break
+                if data.lower()  == "quit":
+                    client_soc.send("Bye client!".encode())
+                    client_soc.close()
+                    break
+                message = input("Enter message: ")
+                if message.lower() == "quit":
+                    client_soc.send("Bye client!".encode())
+                    client_soc.close()
+                    break
+                
+                client_soc.send(message.encode())
+    except Exception as error:
+        print(error)
 
-client_soc , address = server_soc.accept()
-client_soc.send('Hello Client'.encode())
-while True:
-    data = client_soc.recv(1024).decode()
-    print("Client: ",data)
-    message = input("Enter message: ")
-    if not data or message  == "quit":
-        client_soc.send("Bye client!".encode())
-        client_soc.close()
-        break
-    else:
-        client_soc.send(message.encode())
-    
+threads = []
+for i in range(10):
+    t = threading.Thread(target=server)
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+server_soc.close()
