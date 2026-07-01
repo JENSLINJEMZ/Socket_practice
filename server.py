8b0434bd9899ef8fb308296305e92f2661d300ac
JENSLINJEMZ <jenslinjemz123@gmail.com>
2026-07-01 17:50:35 +0530

    add thread handlind to manage clients

--- server.py
+++ server.py
@@ -4,28 +4,50 @@ server_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = "0.0.0.0"
 port = 4444
-
+connected_clients = 0
+lock = threading.Lock()
 server_soc.bind((host,port))
 server_soc.listen(5)
+
 print(f"[*] Server listening on {host}:{port}")
 print(f"[*] Waiting for connections...")
 def server():
+    global connected_clients
     try:
         while True:
             client_soc , address = server_soc.accept()
+            with lock:
+                connected_clients += 1
             print(f"Connected from {address[0]}:{address[1]}")
+            print(f"Clients count:{connected_clients}")
             client_soc.send('Hello Client'.encode())
             while True:
                 data = client_soc.recv(1024).decode()
                 print(f"Client {address[0]}:{address[1]}: ",data)
                 if not data:
-                    print("client not responding...")
-                    client_soc.close()
-                    break
-                if data.lower()  == "quit":
-                    client_soc.send("Bye client!".encode())
-                    client_soc.close()
-                    break
+                    with lock:
+                        connected_clients -=1
+                        print(f"{address[0]} Disconected")
+                        print(f"Clients count:{connected_clients}")
+                        client_soc.close()
+                        break
+                if data.lower() == "quit":
+                    with lock:
+                        if connected_clients == 1:
+                            print("Last client requested shutdown.")
+                            client_soc.send(b"Server shutting down...")
+                            connected_clients -= 1
+                            print(f"Clients count:{connected_clients}")
+                            client_soc.close()
+                            server_soc.close()
+                            return
+                        else:
+                            print(f"{address[0]} disconnected.")
+                            connected_clients -= 1
+                            client_soc.send(b"Bye client!")
+                            print(f"Clients count:{connected_clients}")
+                            client_soc.close()
+                            break
     except Exception as error:
         print(error)
 
